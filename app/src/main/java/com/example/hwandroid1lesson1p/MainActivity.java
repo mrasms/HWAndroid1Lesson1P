86cedc1cf3e60c692e613ab988285117e68fa454
mrasms <shurik96kg@gmail.com>
2022-03-01 02:23:44 +0600

    optimize

--- app/src/main/java/com/example/hwandroid1lesson1p/MainActivity.java
+++ app/src/main/java/com/example/hwandroid1lesson1p/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.hwandroid1lesson1p;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.view.View;
@@ -15,16 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        listener();
+        navigationinit();
     }
 
-    private void listener() {
-        binding.btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
+
+    private void navigationinit() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation,navController);
     }
 
 }
